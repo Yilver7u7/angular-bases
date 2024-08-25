9b9fe8caaa13c2694c303b2191acccb1531abc63
Yilver7u7 <yilversgg@gmail.com>
2024-08-24 22:03:43 -0500

    Add new module with your respective component

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent{
+
+
+
+
+}
+
+
+
+
+
+

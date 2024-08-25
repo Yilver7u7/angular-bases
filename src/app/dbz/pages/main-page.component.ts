8022ce7ad15c8f06edaa777e4d47cbfeebe0fc28
Yilver7u7 <yilversgg@gmail.com>
2024-08-25 12:36:26 -0500

    Improve the ngFor

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
 
 export class MainPageComponent{
 
+
+  public characters: Character[] = [{
+    name: 'Goku',
+    power: 10000,
+  },{
+    name: 'Vegeta',
+    power: 7000,
+  },{
+    name: 'Trunks',
+    power: 6000,
+  },{
+    name: 'Krillin',
+    power: 1000,
+  }
+];
 
 
 
